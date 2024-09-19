339fa71b86d54ba07e35449b128d8c14ad4661c7
Daniel Alnes <daniealn@online.ntnu.no>
2024-09-19 12:22:05 +0200

    TvActiveServer class was added

---
+++ TvActiveServer.java
@@ -0,0 +1,15 @@
+from socket import *
+
+public class TvActiveServer {
+
+    welcomeSocket = socket(AF_INET, SOCK_STREAM)
+    welcomeSocket.bind(('', 6789))
+    welcomeSocket.listen(1)
+    connectionSocket, addr = welcomeSocket.accept()
+    client_message = connectionSocket.recv(1000).decode()
+    print("CLIENT: ", client_message)
+
+    connectionSocket.close()
+    welcomeSocket.close()
+
+}
