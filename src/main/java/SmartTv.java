012ded9f212067b31fb385c5a3410a4e8ed3c5ec
bidha001 <binitd@stud.ntnu.no>
2024-10-07 15:56:33 +0200

    some change

--- src/main/java/SmartTv.java
+++ src/main/java/SmartTv.java
@@ -4,11 +4,6 @@ public class SmartTv extends TvActiveServer {
     private int currentChannel;
     private int totalChannels;
 
-    public static void main(String[] args) throws Exception {
-        SmartTv tv = new SmartTv();
-        tv.runServer();
-    }
-
     public SmartTv() {
         this.isOn = false;
         this.currentChannel = 1;
@@ -24,35 +19,21 @@ public class SmartTv extends TvActiveServer {
     }
 
     private String getChannel() {
-        String Answer;
-        if (!isOn) {
-            Answer = "TV is off.";
-        } else {
-            Answer = "Current channel: " + currentChannel;
+        if (!isOn){
+            return "TV is off.";
+        }else{
+            return "Current channel: " + currentChannel;
         }
-        return Answer;
-    }
-
-    private String getChannels() {
-        String Answer;
-        if (!isOn) {
-            Answer = "TV is off.";
-        } else {
-            Answer = "Total channels: " + totalChannels;
-        }
-        return Answer;
     }
 
     private String setChannel(int channel) {
-        String Answer;
         if (!isOn) {
-            Answer = "TV is off.";
+            return "TV is off.";
         } else if (channel > 0 && channel <= totalChannels) {
             currentChannel = channel;
-            Answer = "Channel set to " + channel;
+            return "Channel set to " + channel;
         } else {
-            Answer = "Invalid channel number.";
+            return  "Invalid channel number.";
         }
-        return Answer;
     }
 }
